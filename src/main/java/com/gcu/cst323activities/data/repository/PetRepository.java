8f5eb6e475b7138231ffd8d5ca3dac6f2922d63a
hviezca <hviezca@my.gcu.edu>
2022-05-02 15:22:16 -0600

    Fixing Deploy Issue

--- src/main/java/com/gcu/cst323activities/data/repository/PetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gcu.cst323activities.data.repository;
-
-import com.gcu.cst323activities.data.entity.PetEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PetRepository extends CrudRepository<PetEntity, Long>  {
-}
